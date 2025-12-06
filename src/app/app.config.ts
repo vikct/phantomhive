c51ef15c00b020f1c766320b5998e287947157fb
vikct <victor.tan@rentguard.com.my>
2025-12-07 07:25:12 +0800

    feat: Integrate Taiga UI for enhanced theming and update social login configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,13 +7,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { CoreModule } from '@core/core.module';
+import { ThemeModule } from '@theme/theme.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideHttpClient(),
-    importProvidersFrom(CoreModule),
+    importProvidersFrom(CoreModule, ThemeModule),
   ],
 };
