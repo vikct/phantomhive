feeaa420a0e759ceeb836a29f1a581bc1a4ec5ef
vikct <victor.tan@rentguard.com.my>
2025-12-21 13:28:34 +0800

    feat: Add environment-specific Firebase configurations and include them in version control.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAeTCb7XF2rV3XmBsTd4xcYSFuzcBA0jfE",
+    authDomain: "fenrisulfr42.firebaseapp.com",
+    projectId: "fenrisulfr42",
+    storageBucket: "fenrisulfr42.firebasestorage.app",
+    messagingSenderId: "599992146606",
+    appId: "1:599992146606:web:6f402f022a52563aae2b3f",
+    measurementId: "G-LMBBLRH37S"
+  }
+};
