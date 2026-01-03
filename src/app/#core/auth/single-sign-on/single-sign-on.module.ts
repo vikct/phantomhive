bb75137ef9bbd44f29dd3fc3abf97f5ce03d8e24
vikct <victor.tan@rentguard.com.my>
2026-01-03 11:31:11 +0800

    feat: Add Facebook SSO integration with improved account linking UI and logic.

--- src/app/#core/auth/single-sign-on/single-sign-on.module.ts
+++ src/app/#core/auth/single-sign-on/single-sign-on.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { provideFirebaseConfig } from '@core/auth/single-sign-on/google/firebase.config';
 import { GoogleSsoButtonComponent } from './google/google-sso-button.component';
 import { GithubSsoButtonComponent } from './github/github-sso-button.component';
+import { FacebookSsoButtonComponent } from './facebook/facebook-sso-button.component';
 
 @NgModule({
-  imports: [CommonModule, GoogleSsoButtonComponent, GithubSsoButtonComponent],
-  exports: [GoogleSsoButtonComponent, GithubSsoButtonComponent],
+  imports: [
+    CommonModule,
+    GoogleSsoButtonComponent,
+    GithubSsoButtonComponent,
+    FacebookSsoButtonComponent,
+  ],
+  exports: [
+    GoogleSsoButtonComponent,
+    GithubSsoButtonComponent,
+    FacebookSsoButtonComponent,
+  ],
   providers: [provideFirebaseConfig()],
 })
 export class SingleSignOnModule {}
